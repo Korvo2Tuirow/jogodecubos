84bf9179c023589806779f5cea09d8183432794b
Korvo2irow <korvo2irow@live.com>
2024-03-23 20:21:36 -0300

    upload na netlify

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,16 +11,6 @@ export const Button = () => {
 
     const [numRandom, setNumRandom] = useState(initialNumRandom);
 
-    const v1 = [1,1,1];
-    const v2 = [2,2,2];
-    const v3 = [3,3,3];
-    const v4 = [4,4,4];
-    const v5 = [5,5,5];
-    const v6 = [6,6,6];
-    const v7 = [7,7,7];
-    const v8 = [8,8,8];
-    const v9 = [9,9,9];
-
     const teste = () => {
 
         setNumRandom([])
@@ -29,21 +19,20 @@ export const Button = () => {
             ...Array.from({ length: 9 }, () => Math.floor(Math.random() * 10 + 1))
         ]);
 
-        NumCtx?.setNumRandomProv(numRandom)
-        console.log(numRandom); 
+        NumCtx?.setNumRandomProv(numRandom)      
         
         const c1 = numRandom.slice(0,3);
         const c2 = numRandom.slice(3,6);
         const c3 = numRandom.slice(6,9);  
-        console.log(c2)       
-    };
-
+                        
+        }
+        
     return (
         <>
-            <button className="m-10 border border-black p-3 rounded-md font-bold bgBtn cursor-pointer shadow-md shadow-black" onClick={teste}>
+            <button className="m-10 border border-black p-3 rounded-md font-bold bgBtn cursor-pointer shadow-md shadow-black hover:scale-125" onClick={teste}>
                 CLIQUE AQUI
             </button>
 
         </>
     );
-};
+    }
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -4,26 +4,26 @@ import { useContext } from "react"
 import { NumContext } from "./ContextNumRandom"
 
 export const Container = () => {
-   
+
     const NumCtx = useContext(NumContext);
-    const c1 = NumCtx?.numRandomProv.slice(0,3);
-    const c2 = NumCtx?.numRandomProv.slice(3,6);
-    const c3 = NumCtx?.numRandomProv.slice(6,9);  
-            
+    const c1 = NumCtx?.numRandomProv.slice(0, 3);
+    const c2 = NumCtx?.numRandomProv.slice(3, 6);
+    const c3 = NumCtx?.numRandomProv.slice(6, 9);
+
     return (
         <div className="border bg-white shadow-lg shadow-black p-4 flex mt-10 border-black max-w-[500px] w-[90%] h-[500px] max-h-[90%] ">
             <div className="w-[33.3%] flex flex-col">
-               {c1?.map(e=>(<div className={`bg${e.toString()} cssDefault`} >{e}</div>))} 
-            </div>
-
-            <div className="w-[33.3%] flex flex-col">        
-                {c2?.map(e=>(<div className={`bg${e.toString()} cssDefault`}>{e}</div>))} 
+                {c1?.map(e => (<div className={`bg${e.toString()} cssDefault`} >{e}</div>))}
             </div>
 
             <div className="w-[33.3%] flex flex-col">
-                {c3?.map(e=>(<div className={`bg${e.toString()} cssDefault`}>{e}</div>))} 
+                {c2?.map(e => (<div className={`bg${e.toString()} cssDefault`}>{e}</div>))}
             </div>
-        
+
+            <div className="w-[33.3%] flex flex-col">
+                {c3?.map(e => (<div className={`bg${e.toString()} cssDefault`}>{e}</div>))}
+            </div>        
+
         </div>
     )
-    }
+}
