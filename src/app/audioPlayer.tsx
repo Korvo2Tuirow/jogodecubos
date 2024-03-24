8929556248c8748ff65d919b82811aed4a83f5fa
Korvo2irow <korvo2irow@live.com>
2024-03-24 19:59:48 -0300

    anima

--- src/app/audioPlayer.tsx
+++ src/app/audioPlayer.tsx
@@ -11,9 +11,9 @@ export const AudioPlayer = ({src, isPlaying}:Props)=>{
     const audioTag = useRef<HTMLAudioElement>(null);
     
     useEffect(()=>{
-        
+        if(isPlaying){
         audioTag.current?.play();
-       
+        }
     })
     
     
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,6 +6,7 @@ import { AudioPlayer } from "@/app/audioPlayer";
 export const Container = () => {
 
     const [playing, setPlaying] = useState(false)
+    const [anima, setAnima] = useState("")
 
 
     const NumCtx = useContext(NumContext);
@@ -13,31 +14,40 @@ export const Container = () => {
     const c2 = NumCtx?.numRandomProv.slice(3, 6);
     const c3 = NumCtx?.numRandomProv.slice(6, 9);
 
-  
 
-    const audio = new Audio('./sound/WhatsappCortos.mp3');
-    audio.play();
+
 
 
     useEffect(() => {
         const v1 = c1?.every(v1 => v1 === c1[0]);
         if (v1 == true) {
             alert("coluna 1")
-            setPlaying(true)
+            setPlaying(!playing)        
+
+            const audio = new Audio('./sound/WhatsappCortos.mp3');
+            audio.play();
         }
 
         const v2 = c2?.every(v2 => v2 === c2[0]);
         if (v2 == true) {
             alert("coluna 2")
-           
+            setPlaying(!playing)             
+
+            const audio = new Audio('./sound/WhatsappCortos.mp3');
+            audio.play();
+
         }
 
         const v3 = c3?.every(v3 => v3 === c3[0]);
         if (v3 == true) {
             alert("coluna 3")
-           
+            setPlaying(!playing)      
+
+            const audio = new Audio('./sound/WhatsappCortos.mp3');
+            audio.play();
+
         }
-        
+
     }, NumCtx?.numRandomProv)
 
 
@@ -45,20 +55,20 @@ export const Container = () => {
     return (
         <div className="border bgBtn shadow-lg shadow-black p-4 flex mt-10 border-black max-w-[500px] w-[90%] h-[500px] max-h-[90%] ">
             <div className="w-[33.3%] flex flex-col ">
-                {c1?.map(e => (<div className={`bg${e.toString()} cssDefault`} >{e}</div>))}
+                {c1?.map(e => (<div className={`bg${e.toString()} cssDefault ${anima}`} >{e}</div>))}
             </div>
 
             <div className="w-[33.3%] flex flex-col">
-                {c2?.map(e => (<div className={`bg${e.toString()}  cssDefault`}>{e}</div>))}
+                {c2?.map(e => (<div className={`bg${e.toString()}  cssDefault ${anima}`}>{e}</div>))}
             </div>
 
             <div className="w-[33.3%] flex flex-col">
-                {c3?.map(e => (<div className={`bg${e.toString()} cssDefault`}>{e}</div>))}
+                {c3?.map(e => (<div className={`bg${e.toString()} cssDefault ${anima}`}>{e}</div>))}
             </div>
-            {playing &&        
+
             <AudioPlayer src="../../public/sound/WhatsappCortos.mp3"
-            isPlaying={playing}/>
-            }
+                isPlaying={playing} />
+
         </div>
     )
 }
