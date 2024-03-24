1f31284a7aad191ed1fabf9259909422463ce26b
Korvo2irow <korvo2irow@live.com>
2024-03-23 23:10:03 -0300

    verificação das colunas

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,6 +3,8 @@ import { useContext, useState } from 'react';
 import { NumContext } from './ContextNumRandom';
 
 
+
+
 export const Button = () => {
 
     const NumCtx = useContext(NumContext);
@@ -20,31 +22,7 @@ export const Button = () => {
             ...Array.from({ length: 9 }, () => Math.floor(Math.random() * 10 + 1))
         ]);
 
-        NumCtx?.setNumRandomProv(numRandom)
-
-        const c1 = numRandom.slice(0, 3);
-        const c2 = numRandom.slice(3, 6);
-        const c3 = numRandom.slice(6, 9);
-
-        const n1 = c1.every(n => n == c1[0])
-        console.log(n1)
-        const n2 = c2.every(n => n == c2[0])
-        console.log(n2)
-        const n3 = c3.every(n => n == c3[0])
-        console.log(n3)
-
-        if (n1 == true) {
-            const audio = new Audio("../../public/sound/Whatsapp_Cortos.mp3");
-            audio.play();
-        }
-        if (n2 == true) {
-            const audio = new Audio("../../public/sound/Whatsapp_Cortos.mp3");
-            audio.play();
-        }
-        if (n3 == true) {
-            const audio = new Audio("../../public/sound/Whatsapp_Cortos.mp3");
-            audio.play();
-        }
+        NumCtx?.setNumRandomProv(numRandom);      
 
     }
 
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 
 
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { NumContext } from "./ContextNumRandom"
 
 export const Container = () => {
@@ -9,6 +9,20 @@ export const Container = () => {
     const c1 = NumCtx?.numRandomProv.slice(0, 3);
     const c2 = NumCtx?.numRandomProv.slice(3, 6);
     const c3 = NumCtx?.numRandomProv.slice(6, 9);
+
+    useEffect(()=>{
+    const v1 = c1?.every(v1=>v1===c1[0]);
+    if(v1 ==true){alert("coluna 1")}
+
+    const v2 = c2?.every(v2=>v2===c2[0]);
+    if(v2 ==true){alert("coluna 2")}
+
+    const v3 = c3?.every(v3=>v3===c3[0]);
+    if(v3 ==true){alert("coluna 3")}
+
+    }, NumCtx?.numRandomProv)
+
+
 
     return (
         <div className="border bgBtn shadow-lg shadow-black p-4 flex mt-10 border-black max-w-[500px] w-[90%] h-[500px] max-h-[90%] ">
