ae6ac262b78ee97956a845362317edcb0403fd6a
Korvo2irow <korvo2irow@live.com>
2024-03-24 18:58:05 -0300

    tentando colocar som

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import { NumRandomProvider } from "@/components/ContextNumRandom";
 import { Button } from "@/components/button";
 import { Container } from "@/components/container";
 
+
+
+
 const Page = () => { 
+
+     
 
   return (
     <div className="flex flex-col pb-10 justify-center items-center h-screen w-full bg-gradient-to-r from-sky-600 to-emerald-400">
@@ -11,6 +16,8 @@ const Page = () => {
     <NumRandomProvider>
         <Container />
         <Button />
+       
+       
     </NumRandomProvider>
      
     </div>
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,26 +1,35 @@
 
-
-import { useContext, useEffect } from "react"
-import { NumContext } from "./ContextNumRandom"
+import React, { useContext, useEffect, useState } from "react";
+import { NumContext } from "./ContextNumRandom";
+import { AudioPlayer } from "@/app/audioPlayer";
 
 export const Container = () => {
+
+    const [playing, setPlaying] = useState(false)
+
 
     const NumCtx = useContext(NumContext);
     const c1 = NumCtx?.numRandomProv.slice(0, 3);
     const c2 = NumCtx?.numRandomProv.slice(3, 6);
     const c3 = NumCtx?.numRandomProv.slice(6, 9);
 
+  
+
+    const audio = new Audio('./sound/WhatsappCortos.mp3');
+    audio.play();
+
+
     useEffect(() => {
         const v1 = c1?.every(v1 => v1 === c1[0]);
         if (v1 == true) {
-      alert("coluna 1")
-           
+            alert("coluna 1")
+            setPlaying(true)
         }
 
         const v2 = c2?.every(v2 => v2 === c2[0]);
         if (v2 == true) {
             alert("coluna 2")
-                       
+           
         }
 
         const v3 = c3?.every(v3 => v3 === c3[0]);
@@ -28,7 +37,7 @@ export const Container = () => {
             alert("coluna 3")
            
         }
-
+        
     }, NumCtx?.numRandomProv)
 
 
@@ -46,8 +55,10 @@ export const Container = () => {
             <div className="w-[33.3%] flex flex-col">
                 {c3?.map(e => (<div className={`bg${e.toString()} cssDefault`}>{e}</div>))}
             </div>
-        
-
+            {playing &&        
+            <AudioPlayer src="../../public/sound/WhatsappCortos.mp3"
+            isPlaying={playing}/>
+            }
         </div>
     )
 }
