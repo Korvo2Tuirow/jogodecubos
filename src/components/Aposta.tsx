180413964ccbcd5211255e287f4122794b334dbe
Korvo2irow <korvo2irow@live.com>
2024-03-29 17:29:25 -0300

    aposta

--- src/components/Aposta.tsx
+++ src/components/Aposta.tsx
@@ -1,20 +1,25 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { NumContext } from "./ContextNumRandom";
 
+type Props={
+    newValor: number;
+    setNewValor: (e:number)=>void;
+}
 
 
 export const Aposta = ()=>{
     
     const [newValor, setNewValor] = useState();
-    const valorCtx = useContext(NumContext);
+    const Ctx = useContext(NumContext);
     
     const modal = () =>{
         const exit = document.getElementById("modal");
         exit?.classList.remove("hidden");
+
     }
     return(
         <div className="container bg-white rounded-md w-[90%] max-w-[500px] p-3 flex justify-between shadow-lg shadow-black">
-            <p>Banca: {valorCtx?.valor} </p>
+            <p>Banca: {Ctx?.valor} </p>
             <button onClick={modal}>Apostar</button> 
         </div>
     )
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,8 +6,8 @@ export const Modal = () => {
 
     const valorCtx = useContext(NumContext);
 
-    const [valor, setValor] =useState("");
-   
+    const [valor, setValor] = useState("");
+
     const add = () => {
         const newValor = parseFloat(valor)
 
@@ -15,14 +15,15 @@ export const Modal = () => {
 
             const exit = document.getElementById("modal");
             exit?.classList.add("hidden");
-           valorCtx?.setValor(newValor);          
+            valorCtx?.setValor(newValor);  
+           
         }
     }
 
     return (
-        <div id="modal" className="w-screen h-screen absolute flex justify-center items-center z-50 bg-black/90">
+        <div id="modal" className="w-screen h-screen absolute flex justify-center items-center z-50 bg-black/90">      
 
-            <div className={`border-4 justify-center items-center top-[20%] p-4 flex flex-col m-10  border-white bgBtn absolute rounded-2xl w-[90%] max-w-[500px] h-[100%] max-h-[200px]`}>
+            <div id="addCash" className={`border-4 justify-center items-center top-[20%] p-4 flex flex-col m-10  border-white bgBtn absolute rounded-2xl w-[90%] max-w-[500px] h-[100%] max-h-[200px]`}>
 
                 <input type="number"
                     min={1}
@@ -30,7 +31,8 @@ export const Modal = () => {
                     step="0.01"
                     placeholder="QUER APOSTAR QUANTO ?"
                     value={valor}
-                    onChange={e =>setValor((e.target.value))}
+                    onChange={e => setValor((e.target.value))}
+                    autoFocus
                 />
 
                 <button onClick={add} className="p-3 border font-bold hover:scale-110 border-black rounded-md mt-5 bg-[#57f5e0]" >APOSTE</button>
